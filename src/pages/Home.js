ac79231aae200234d4f5e5b45aec047ab8b83229
Ali zareshahi <alititi96@gmail.com>
2021-12-13 15:10:52 +0330

    add font

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,8 +52,7 @@ class Home extends Component {
   handleBack = (e) => {
     e.preventDefault();
     let current = this.state.currentDir;
-    if(current.length>1)
-        current.pop();
+    if (current.length > 1) current.pop();
     console.log("curr:", current);
     this.setState({ currentDir: current });
     this.getLocalLastDirectoryContent();
@@ -61,6 +60,11 @@ class Home extends Component {
 
   clickItem = (e, data) => {
     e.preventDefault();
+    if (data.indexOf(".jpg") !== -1) {
+      //image
+      return;
+    }
+
     let current = this.state.currentDir;
     current.push(data);
     this.setState({ currentDir: current });
@@ -72,36 +76,68 @@ class Home extends Component {
       <>
         <AppBar>
           <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={(e) => {
-                this.handleBack(e);
-              }}
-              aria-label="close"
+            {this.state.currentDir.length > 1 ? (
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={(e) => {
+                  this.handleBack(e);
+                }}
+                aria-label="close"
+              >
+                <ArrowBackIcon />
+              </IconButton>
+            ) : (
+              ""
+            )}
+            <div className="right-title">
+            <h6
+              style={{ color: "#ffffff", marginTop: "5px" }}
             >
-              <ArrowBackIcon />
-            </IconButton>
+              {this.state.currentDir[this.state.currentDir.length-1]}
+            </h6>
+          </div>
           </Toolbar>
         </AppBar>
-        <List style={{ marginTop: "10%" }}>
+        <List className="d-flex flex-column mt-5">
           {this.state.contents.length !== 0
             ? this.state.contents.map((item, key) => {
                 return (
-                  <Card key={key} className={"mt-2"}>
-                    <a
-                      href={configs.API_URL+this.state.currentDir.join('\\')+'\\'+item}
-                      onClick={(e) => {
-                        this.clickItem(e, item);
-                      }}
+                  <a
+                    href={
+                      configs.API_URL +
+                      this.state.currentDir.join("\\") +
+                      "\\" +
+                      item
+                    }
+                    onClick={(e) => {
+                      this.clickItem(e, item);
+                    }}
+                  >
+                    <Card
+                      style={{ direction: "rtl" }}
+                      key={key}
+                      className={"d-flex flex-row m-2 p-3"}
                     >
-                      {
-                          item.indexOf('.jpg')!==-1?
-                          <img height={150} alt={item} src={configs.API_URL+this.state.currentDir.join('\\')+'\\'+item} />
-                          :item
-                      }
-                    </a>
-                  </Card>
+                      {item.indexOf(".jpg") !== -1 ? (
+                        <div style={{textAlign:'center',width:'100%'}}>
+                            <img
+                          style={{width:'100%'}}
+                          alt={item}
+                          src={
+                            configs.API_URL +
+                            this.state.currentDir.join("\\") +
+                            "\\" +
+                            item
+                          }
+                        />
+                        <h3>{item.replace('.jpg','')}</h3>
+                        </div>
+                      ) : (
+                        <label style={{ direction: "rtl" }}>{item}</label>
+                      )}
+                    </Card>
+                  </a>
                 );
               })
             : ""}
