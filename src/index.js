545a8f7bcf9c8b5fea35c7ea6763d5a1e8e7387b
Ali zareshahi <alititi96@gmail.com>
2021-12-13 11:50:27 +0330

    init3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import configs from './config';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
 import { StylesProvider, ThemeProvider, jssPreset } from "@material-ui/styles";
 import { createTheme } from '@mui/material/styles';
 import { create } from "jss";
 import rtl from "jss-rtl";
-
+import axios from 'axios';
 
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 const theme = createTheme({
   direction: "rtl",
 });
+
+axios.defaults.baseURL = configs.API_URL;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 ReactDOM.render(
   <React.StrictMode>
